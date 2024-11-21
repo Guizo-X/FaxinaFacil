8d24584b7b4c701c20d0423f6e87b5c34307fc99
ViniiAssis <144054711+GuiLaurentino@users.noreply.github.com>
2024-11-21 20:45:44 -0300

    att

--- fashine/config/database.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('fashine', 'guizo', 'Lolzinho!123', {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-});
-
-module.exports = sequelize;
--- js/inicio.js
+++ js/inicio.js
@@ -4,8 +4,28 @@ body.style.width = "100%";
 body.style.display = "flex";
 body.style.alignItems = "center";
 body.style.flexDirection = "column";
-body.style.backgroundColor = "gray";
 
+const negativeMode = document.createElement("button");
+negativeMode.style.width = "100px";
+negativeMode.style.height = "100px";
+negativeMode.style.backgroundColor = "Red";
+negativeMode.style.color = "white";
+negativeMode.textContent = "Modo Noturno";
+negativeMode.addEventListener("click", function(){
+    body.style.backgroundColor = "black";
+})
+body.appendChild(negativeMode);
+
+const lightMode = document.createElement("button");
+lightMode.style.width = "100px";
+lightMode.style.height = "100px";
+lightMode.style.backgroundColor = "Red";
+lightMode.style.color = "white";
+lightMode.textContent = "Modo Claro";
+lightMode.addEventListener("click", function(){
+    body.style.backgroundColor = "white";
+})
+body.appendChild(lightMode);
 
 const divPai = document.createElement("div");
 divPai.style.width = "250px";
@@ -22,279 +42,6 @@ TodosStories1.style.height = "80px";
 TodosStories1.style.display = "flex";
 TodosStories1.style.justifyContent = "center";
 body.appendChild(TodosStories1)
-
-
-const usuario = document.createElement("div");
-usuario.style.width = "70px";
-usuario.style.height = "70px";
-usuario.style.backgroundColor = "gray";
-usuario.style.borderRadius = "50%";
-usuario.style.borderTop = "5px solid #7BDAAF";
-usuario.style.marginLeft = "30px";
-usuario.style.borderRight = "5px solid yellow";
-usuario.style.borderLeft = "5px solid orange";
-usuario.style.borderBottom = "5px solid cyan";
-usuario.style.backgroundImage = "url(../assets/faxineiro17.jpg)"
-usuario.style.backgroundSize = "cover";
-usuario.style.backgroundPosition = "-20px";
-TodosStories1.appendChild(usuario)
-
-
-const stories2 = document.createElement("div");
-stories2.style.width = "70px";
-stories2.style.height = "70px";
-stories2.style.backgroundColor = "gray";
-stories2.style.borderRadius = "50%";
-stories2.style.borderTop = "5px solid #7BDAAF";
-stories2.style.borderRight = "5px solid yellow";
-stories2.style.marginLeft = "30px";
-stories2.style.borderLeft = "5px solid orange";
-stories2.style.borderBottom = "5px solid cyan";
-stories2.style.backgroundImage = "url(../assets/faxineiro16.jpg)"
-stories2.style.backgroundSize = "cover";
-stories2.style.backgroundPosition = "-23px";
-TodosStories1.appendChild(stories2)
-
-
-const stories3 = document.createElement("div");
-stories3.style.width = "70px";
-stories3.style.height = "70px";
-stories3.style.backgroundColor = "gray";
-stories3.style.borderRadius = "50%";
-stories3.style.borderTop = "5px solid #7BDAAF";
-stories3.style.borderRight = "5px solid yellow";
-stories3.style.marginLeft = "30px";
-stories3.style.borderLeft = "5px solid orange";
-stories3.style.borderBottom = "5px solid cyan";
-stories3.style.backgroundImage = "url(../assets/faxineiro15.jpg)"
-stories3.style.backgroundSize = "cover";
-stories3.style.backgroundPosition = "-20px";
-TodosStories1.appendChild(stories3)
-
-
-const stories4 = document.createElement("div");
-stories4.style.width = "70px";
-stories4.style.height = "70px";
-stories4.style.backgroundColor = "gray";
-stories4.style.borderRadius = "50%";
-stories4.style.borderTop = "5px solid #7BDAAF";
-stories4.style.borderRight = "5px solid yellow";
-stories4.style.marginLeft = "30px";
-stories4.style.borderLeft = "5px solid orange";
-stories4.style.borderBottom = "5px solid cyan";
-stories4.style.backgroundImage = "url(../assets/faxineiro14.jpg)"
-stories4.style.backgroundSize = "cover";
-TodosStories1.appendChild(stories4)
-
-
-const stories5 = document.createElement("div");
-stories5.style.width = "70px";
-stories5.style.height = "70px";
-stories5.style.backgroundColor = "gray";
-stories5.style.borderRadius = "50%";
-stories5.style.borderTop = "5px solid #7BDAAF";
-stories5.style.borderRight = "5px solid yellow";
-stories5.style.marginLeft = "30px";
-stories5.style.borderLeft = "5px solid orange";
-stories5.style.borderBottom = "5px solid cyan";
-stories5.style.backgroundImage = "url(../assets/faxineiro13.jpg)"
-stories5.style.backgroundSize = "cover";
-stories5.style.backgroundPosition = "-30px";
-TodosStories1.appendChild(stories5)
-
-
-const stories6 = document.createElement("div");
-stories6.style.width = "70px";
-stories6.style.height = "70px";
-stories6.style.backgroundColor = "gray";
-stories6.style.borderRadius = "50%";
-stories6.style.borderTop = "5px solid #7BDAAF";
-stories6.style.borderRight = "5px solid yellow";
-stories6.style.marginLeft = "30px";
-stories6.style.borderLeft = "5px solid orange";
-stories6.style.borderBottom = "5px solid cyan";
-stories6.style.backgroundImage = "url(../assets/faxineiro12.jpg)"
-stories6.style.backgroundSize = "cover";
-stories6.style.backgroundPosition = "-35px";
-TodosStories1.appendChild(stories6)
-
-
-const stories7 = document.createElement("div");
-stories7.style.width = "70px";
-stories7.style.height = "70px";
-stories7.style.backgroundColor = "gray";
-stories7.style.borderRadius = "50%";
-stories7.style.borderTop = "5px solid #7BDAAF";
-stories7.style.borderRight = "5px solid yellow";
-stories7.style.marginLeft = "30px";
-stories7.style.borderLeft = "5px solid orange";
-stories7.style.borderBottom = "5px solid cyan";
-stories7.style.backgroundImage = "url(../assets/faxineiro11.jpg)"
-stories7.style.backgroundSize = "cover";
-TodosStories1.appendChild(stories7)
-
-
-const stories8 = document.createElement("div");
-stories8.style.width = "70px";
-stories8.style.height = "70px";
-stories8.style.backgroundColor = "gray";
-stories8.style.borderRadius = "50%";
-stories8.style.borderTop = "5px solid #7BDAAF";
-stories8.style.borderRight = "5px solid yellow";
-stories8.style.marginLeft = "30px";
-stories8.style.borderLeft = "5px solid orange";
-stories8.style.borderBottom = "5px solid cyan";
-stories8.style.backgroundImage = "url(../assets/faxineiro10.jpg)"
-stories8.style.backgroundSize = "cover";
-stories8.style.backgroundPosition = "-20px";
-TodosStories1.appendChild(stories8)
-
-
-const stories9 = document.createElement("div");
-stories9.style.width = "70px";
-stories9.style.height = "70px";
-stories9.style.backgroundColor = "gray";
-stories9.style.borderRadius = "50%";
-stories9.style.borderTop = "5px solid #7BDAAF";
-stories9.style.borderRight = "5px solid yellow";
-stories9.style.marginLeft = "30px";
-stories9.style.borderLeft = "5px solid orange";
-stories9.style.borderBottom = "5px solid cyan";
-stories9.style.backgroundImage = "url(../assets/faxineira9.jpg)"
-stories9.style.backgroundSize = "cover";
-stories9.style.backgroundPosition = "-18px";
-TodosStories1.appendChild(stories9)
-
-
-const stories10 = document.createElement("div");
-stories10.style.width = "70px";
-stories10.style.height = "70px";
-stories10.style.backgroundColor = "gray";
-stories10.style.borderRadius = "50%";
-stories10.style.borderTop = "5px solid #7BDAAF";
-stories10.style.borderRight = "5px solid yellow";
-stories10.style.marginLeft = "30px";
-stories10.style.borderLeft = "5px solid orange";
-stories10.style.borderBottom = "5px solid cyan";
-stories10.style.backgroundImage = "url(../assets/faxineira8.jpg)"
-stories10.style.backgroundSize = "cover";
-stories10.style.backgroundPosition = "-20px";
-TodosStories1.appendChild(stories10)
-
-
-const stories11 = document.createElement("div");
-stories11.style.width = "70px";
-stories11.style.height = "70px";
-stories11.style.backgroundColor = "gray";
-stories11.style.borderRadius = "50%";
-stories11.style.borderTop = "5px solid #7BDAAF";
-stories11.style.borderRight = "5px solid yellow";
-stories11.style.marginLeft = "30px";
-stories11.style.borderLeft = "5px solid orange";
-stories11.style.borderBottom = "5px solid cyan";
-stories11.style.backgroundImage = "url(../assets/faxineira7.jpg)"
-stories11.style.backgroundSize = "cover";
-TodosStories1.appendChild(stories11)
-
-
-const stories12 = document.createElement("div");
-stories12.style.width = "70px";
-stories12.style.height = "70px";
-stories12.style.backgroundColor = "gray";
-stories12.style.borderRadius = "50%";
-stories12.style.borderTop = "5px solid #7BDAAF";
-stories12.style.borderRight = "5px solid yellow";
-stories12.style.marginLeft = "30px";
-stories12.style.borderLeft = "5px solid orange";
-stories12.style.borderBottom = "5px solid cyan";
-stories12.style.backgroundImage = "url(../assets/faxineira6.jpg)"
-stories12.style.backgroundSize = "cover";
-TodosStories1.appendChild(stories12)
-
-
-const stories13 = document.createElement("div");
-stories13.style.width = "70px";
-stories13.style.height = "70px";
-stories13.style.backgroundColor = "gray";
-stories13.style.borderRadius = "50%";
-stories13.style.borderTop = "5px solid #7BDAAF";
-stories13.style.borderRight = "5px solid yellow";
-stories13.style.marginLeft = "30px";
-stories13.style.borderLeft = "5px solid orange";
-stories13.style.borderBottom = "5px solid cyan";
-stories13.style.backgroundImage = "url(../assets/faxineira5.jpg)"
-stories13.style.backgroundSize = "cover";
-TodosStories1.appendChild(stories13)
-
-
-const stories14 = document.createElement("div");
-stories14.style.width = "70px";
-stories14.style.height = "70px";
-stories14.style.backgroundColor = "gray";
-stories14.style.borderRadius = "50%";
-stories14.style.borderTop = "5px solid #7BDAAF";
-stories14.style.borderRight = "5px solid yellow";
-stories14.style.marginLeft = "30px";
-stories14.style.borderLeft = "5px solid orange";
-stories14.style.borderBottom = "5px solid cyan";
-stories14.style.backgroundImage = "url(../assets/faxineira4.jpg)"
-stories14.style.backgroundSize = "cover";
-stories14.style.backgroundPosition = "-35px";
-TodosStories1.appendChild(stories14)
-
-
-const stories15 = document.createElement("div");
-stories15.style.width = "70px";
-stories15.style.height = "70px";
-stories15.style.backgroundColor = "gray";
-stories15.style.borderRadius = "50%";
-stories15.style.borderTop = "5px solid #7BDAAF";
-stories15.style.borderRight = "5px solid yellow";
-stories15.style.marginLeft = "30px";
-stories15.style.borderLeft = "5px solid orange";
-stories15.style.borderBottom = "5px solid cyan";
-stories15.style.backgroundImage = "url(../assets/faxineira3.jpg)"
-stories15.style.backgroundSize = "cover";
-TodosStories1.appendChild(stories15)
-
-
-const stories16 = document.createElement("div");
-stories16.style.width = "70px";
-stories16.style.height = "70px";
-stories16.style.backgroundColor = "gray";
-stories16.style.borderRadius = "50%";
-stories16.style.borderTop = "5px solid #7BDAAF";
-stories16.style.borderRight = "5px solid yellow";
-stories16.style.marginLeft = "30px";
-stories16.style.borderLeft = "5px solid orange";
-stories16.style.borderBottom = "5px solid cyan";
-stories16.style.backgroundImage = "url(../assets/faxineira2.jpg)"
-stories16.style.backgroundSize = "cover";
-TodosStories1.appendChild(stories16)
-
-
-const stories17 = document.createElement("div");
-stories17.style.width = "70px";
-stories17.style.height = "70px";
-stories17.style.backgroundColor = "gray";
-stories17.style.borderRadius = "50%";
-stories17.style.borderTop = "5px solid #7BDAAF";
-stories17.style.borderRight = "5px solid yellow";
-stories17.style.marginLeft = "30px";
-stories17.style.borderLeft = "5px solid orange";
-stories17.style.borderBottom = "5px solid cyan";
-stories17.style.backgroundImage = "url(../assets/faxineira.jpg)"
-stories17.style.backgroundSize = "cover";
-stories17.style.backgroundPosition = "-25px"
-TodosStories1.appendChild(stories17)
-
-
-const divLinhaStories = document.createElement("div");
-divLinhaStories.style.backgroundColor = "black";
-divLinhaStories.style.marginTop = "10px";
-divLinhaStories.style.width = "102%";
-divLinhaStories.style.height = "1px";
-body.appendChild(divLinhaStories)
 
 
 const textoFaxina = document.createElement("h1");
@@ -671,8 +418,6 @@ recarregarPag.addEventListener("click", function() {
 });
 
 divfilho.appendChild(recarregarPag);
-
-
 
 })
 divfilho.appendChild(outros)
